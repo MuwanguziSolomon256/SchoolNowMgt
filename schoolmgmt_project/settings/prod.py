9edcc19c399d5fce62d0712a2b919813a7e59673
unknown <victorelite101@gmail.com>
2026-05-30 17:11:17 +0300

    Switch to SQLite for PythonAnywhere deployment

--- schoolmgmt_project/settings/prod.py
+++ schoolmgmt_project/settings/prod.py
@@ -14,17 +14,8 @@ ALLOWED_HOSTS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':     config('DB_NAME', default='school_db'),
-        'USER':     config('DB_USER', default='school_user'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST':     config('DB_HOST', default='localhost'),
-        'PORT':     config('DB_PORT', default='5432'),
-        'CONN_MAX_AGE': 0,  # Disable connection pooling on Render
-        'OPTIONS': {
-            'connect_timeout': 10,
-        },
-        'ATOMIC_REQUESTS': False,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
